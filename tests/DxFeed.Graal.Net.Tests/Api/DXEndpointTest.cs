9e1ea80d0002b732d9846e5d7f5f4e02cc5484d5
stdcion <stdcion@gmail.com>
2022-12-21 13:19:08 +0300

    FEATURE: Add Endpoint unit tests (#2)
    * test: add unit test for DXEndpoint.Builder
    * refactor: move DXEndpointTest to Api folder
    * style: auto-format DxFeed.Graal.Get.Tests.cproj
    * style: move test method up
    * test: add new test methods
    * style: fix file encoding
    * test: add endpoint listener test

---
+++ tests/DxFeed.Graal.Net.Tests/Api/DXEndpointTest.cs
@@ -0,0 +1,101 @@
+// <copyright file="DXEndpointTest.cs" company="Devexperts LLC">
+// Copyright © 2022 Devexperts LLC. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+using DxFeed.Graal.Net.Native.ErrorHandling;
+using static DxFeed.Graal.Net.Api.DXEndpoint;
+using static DxFeed.Graal.Net.Api.DXEndpoint.Role;
+
+namespace DxFeed.Graal.Net.Tests.Api;
+
+[TestFixture]
+public class DXEndpointTest
+{
+    [Test]
+    public void ThrowExceptionWhenDisposed()
+    {
+        var endpoint = Create();
+        endpoint.Dispose();
+        Assert.Throws<JavaException>(() => endpoint.GetFeed());
+        Assert.Throws<JavaException>(() => endpoint.GetPublisher());
+    }
+
+    [Test]
+    public void MultipleDisposeNotThrowException()
+    {
+        var endpoint = Create();
+        Assert.DoesNotThrow(() => endpoint.Dispose());
+        Assert.DoesNotThrow(() => endpoint.Dispose());
+    }
+
+    [Test]
+    public void DefaultEndpointRoleIsFeed() =>
+        Assert.Multiple(() =>
+        {
+            Assert.That(Instance.GetRole(), Is.EqualTo(Feed));
+            Assert.That(Create().GetRole(), Is.EqualTo(Feed));
+        });
+
+    [Test]
+    public void CreateMethodBuildCorrectEndpointRole() =>
+        Assert.Multiple(() =>
+        {
+            Assert.That(Create(Feed).GetRole(), Is.EqualTo(Feed));
+            Assert.That(Create(OnDemandFeed).GetRole(), Is.EqualTo(OnDemandFeed));
+            Assert.That(Create(StreamFeed).GetRole(), Is.EqualTo(StreamFeed));
+            Assert.That(Create(Publisher).GetRole(), Is.EqualTo(Publisher));
+            Assert.That(Create(StreamPublisher).GetRole(), Is.EqualTo(StreamPublisher));
+            Assert.That(Create(LocalHub).GetRole(), Is.EqualTo(LocalHub));
+        });
+
+    [Test]
+    public void UnsupportedRoleThrowException() =>
+        Assert.Throws<JavaException>(() => Create((Role)100500));
+
+    [Test]
+    public void GetInstanceReturnsSameObject()
+    {
+        foreach (var role in (Role[])Enum.GetValues(typeof(Role)))
+        {
+            var endpoint = GetInstance(role);
+            Assert.That(GetInstance(role), Is.EqualTo(endpoint));
+        }
+    }
+
+    [Test]
+    public void GetFeedReturnsSameObject()
+    {
+        var feed = Instance.GetFeed();
+        Assert.That(feed, Is.EqualTo(Instance.GetFeed()));
+    }
+
+    [Test]
+    public void GetGePublisherReturnsSameObject()
+    {
+        var publisher = Instance.GetPublisher();
+        Assert.That(publisher, Is.EqualTo(Instance.GetPublisher()));
+    }
+
+    [Test]
+    public void CheckDisposeCallClose()
+    {
+        var countdownEvent = new CountdownEvent(1);
+        var endpoint = Create(Feed);
+        var currentState = endpoint.GetState();
+        endpoint.AddStateChangeListener((_, newState) =>
+        {
+            currentState = newState;
+            countdownEvent.Signal();
+        });
+
+        endpoint.Dispose();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(countdownEvent.Wait(new TimeSpan(0, 0, 3)), Is.True);
+            Assert.That(currentState, Is.EqualTo(State.Closed));
+        });
+    }
+}
