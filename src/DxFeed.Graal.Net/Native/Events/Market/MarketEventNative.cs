22d52601bf3210c16a0bdf6e1694ead10f27594f
Konstantin Ivaschenko <stdcion@gmail.com>
2023-01-13 20:29:45 +0300

    feat: add GreeksNative

--- src/DxFeed.Graal.Net/Native/Events/Market/MarketEventNative.cs
+++ src/DxFeed.Graal.Net/Native/Events/Market/MarketEventNative.cs
@@ -1,4 +1,4 @@
-// <copyright file="MarketEvent.cs" company="Devexperts LLC">
+// <copyright file="MarketEventNative.cs" company="Devexperts LLC">
 // Copyright © 2022 Devexperts LLC. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 // If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
@@ -14,7 +14,7 @@ namespace DxFeed.Graal.Net.Native.Events.Market;
 /// Must be included at the beginning of every market event structure.
 /// </summary>
 [StructLayout(LayoutKind.Sequential)]
-internal readonly struct MarketEvent
+internal readonly struct MarketEventNative
 {
     public readonly BaseEventNative Base;
     public readonly nint EventSymbol; // A null-terminated UTF-8 string.
