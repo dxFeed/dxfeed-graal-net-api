143c8caa70bbdb62b513ba6d4c83d3ec0cc8f230
Konstantin Ivaschenko <stdcion@gmail.com>
2023-02-15 21:23:41 +0300

    refactor: rename Candle CheckInAttribute

--- src/DxFeed.Graal.Net/Events/Candles/CandleExchange.cs
+++ src/DxFeed.Graal.Net/Events/Candles/CandleExchange.cs
@@ -66,7 +66,7 @@ public class CandleExchange : ICandleSymbolProperty
     /// </summary>
     /// <param name="candleSymbol">The candle symbol.</param>
     /// <exception cref="InvalidOperationException">If used outside of internal initialization logic.</exception>
-    public void CheckInAttributeCore(CandleSymbol candleSymbol)
+    public void CheckInAttribute(CandleSymbol candleSymbol)
     {
         if (candleSymbol.Exchange != null)
         {
