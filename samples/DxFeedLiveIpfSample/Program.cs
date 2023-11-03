46628884ad4891f88affaab9de98adbd561ad378
stdcion <stdcion@gmail.com>
2023-11-03 16:45:23 +0300

    Add IpfConnection (#44)

---
+++ samples/DxFeedLiveIpfSample/Program.cs
@@ -0,0 +1,65 @@
+﻿// <copyright file="Program.cs" company="Devexperts LLC">
+// Copyright © 2022 Devexperts LLC. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using DxFeed.Graal.Net.Ipf;
+using DxFeed.Graal.Net.Ipf.Live;
+
+namespace DxFeed.Graal.Net.Samples;
+
+internal abstract class Program
+{
+    private static readonly string DXFEED_IPF_URL = "https://demo:demo@tools.dxfeed.com/ipf";
+
+    public static async Task Main(string[] args)
+    {
+        if (args.Length > 1) {
+            Console.WriteLine("usage: DXFeedLiveIpfSample [<ipf-url>]");
+            Console.WriteLine("where: <ipf-url>  is URL for the instruments profiles, default: " + DXFEED_IPF_URL);
+            return;
+        }
+
+        var url = (args.Length > 0) ? args[0] : DXFEED_IPF_URL;
+
+        var collector = new InstrumentProfileCollector();
+        var connection = InstrumentProfileConnection.CreateConnection(url, collector);
+        // Update period can be used to re-read IPF files, not needed for services supporting IPF "live-update"
+        connection.SetUpdatePeriod(60_000L);
+        connection.Start();
+
+        // Data model to keep all instrument profiles mapped by their ticker symbol
+        var profiles = new ConcurrentDictionary<string, InstrumentProfile>();
+
+        // It is possible to add listener after connection is started - updates will not be missed in this case
+        collector.AddUpdateListener(instruments =>
+        {
+            Console.WriteLine("\nInstrument Profiles:");
+            // We can observe REMOVED elements - need to add necessary filtering
+            // See javadoc for InstrumentProfileCollector for more details
+
+            // (1) We can either process instrument profile updates manually
+            instruments.ForEach(profile =>
+            {
+                if (InstrumentProfileType.REMOVED.Name.Equals(profile.GetType().Name, StringComparison.Ordinal)) {
+                    // Profile was removed - remove it from our data model
+                    profiles.Remove(profile.Symbol, out _);
+                } else {
+                    // Profile was updated - collector only notifies us if profile was changed
+                    profiles.TryAdd(profile.Symbol, profile);
+                }
+            });
+            Console.WriteLine("Total number of profiles (1): " + profiles.Count);
+            Console.WriteLine("Last modified: " + DateTimeOffset.FromUnixTimeMilliseconds(collector.GetLastUpdateTime()));
+        });
+
+
+        await Task.Delay(Timeout.Infinite);
+    }
+}
---
+++ src/DxFeed.Graal.Net/Native/Ipf/InstrumentProfileConnectionHandle.cs
@@ -0,0 +1,130 @@
+// <copyright file="InstrumentProfileConnectionHandle.cs" company="Devexperts LLC">
+// Copyright © 2022 Devexperts LLC. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+using System.Runtime.InteropServices;
+using DxFeed.Graal.Net.Ipf.Live;
+using DxFeed.Graal.Net.Native.ErrorHandling;
+using DxFeed.Graal.Net.Native.Interop;
+
+namespace DxFeed.Graal.Net.Native.Ipf;
+
+internal class InstrumentProfileConnectionHandle : JavaHandle
+{
+    public static InstrumentProfileConnectionHandle Create(string address, InstrumentProfileCollectorHandle instrumentProfileCollector) =>
+        ErrorCheck.NativeCall(CurrentThread, Import.CreateConnection(CurrentThread, address, instrumentProfileCollector));
+
+
+    public string? GetAddress()
+    {
+        using var address = ErrorCheck.NativeCall(CurrentThread, Import.GetAddress(CurrentThread, this));
+        return address.ToString();
+    }
+
+    public long GetUpdatePeriod() =>
+        ErrorCheck.NativeCall(CurrentThread, Import.GetUpdatePeriod(CurrentThread, this));
+
+    public void SetUpdatePeriod(long updatePeriod) =>
+        ErrorCheck.NativeCall(CurrentThread, Import.SetUpdatePeriod(CurrentThread, this, updatePeriod));
+
+    public long GetLastModified() =>
+        ErrorCheck.NativeCall(CurrentThread, Import.GetLasModified(CurrentThread, this));
+
+    public void Start() =>
+        ErrorCheck.NativeCall(CurrentThread, Import.Start(CurrentThread, this));
+
+    public new void Close() =>
+        ErrorCheck.NativeCall(CurrentThread, Import.Close(CurrentThread, this));
+
+    private static class Import
+    {
+        [DllImport(
+            ImportInfo.DllName,
+            CallingConvention = CallingConvention.Cdecl,
+            CharSet = CharSet.Ansi,
+            ExactSpelling = true,
+            BestFitMapping = false,
+            ThrowOnUnmappableChar = true,
+            EntryPoint = "dxfg_InstrumentProfileConnection_createConnection")]
+        public static extern InstrumentProfileConnectionHandle CreateConnection(
+            nint thread,
+            [MarshalAs(UnmanagedType.LPUTF8Str)] string address,
+            InstrumentProfileCollectorHandle instrumentProfileCollector);
+
+        [DllImport(
+            ImportInfo.DllName,
+            CallingConvention = CallingConvention.Cdecl,
+            CharSet = CharSet.Ansi,
+            ExactSpelling = true,
+            BestFitMapping = false,
+            ThrowOnUnmappableChar = true,
+            EntryPoint = "dxfg_InstrumentProfileConnection_getAddress")]
+        public static extern JavaStringHandle GetAddress(
+            nint thread,
+            InstrumentProfileConnectionHandle instrumentProfileConnection);
+
+
+        [DllImport(
+            ImportInfo.DllName,
+            CallingConvention = CallingConvention.Cdecl,
+            CharSet = CharSet.Ansi,
+            ExactSpelling = true,
+            BestFitMapping = false,
+            ThrowOnUnmappableChar = true,
+            EntryPoint = "dxfg_InstrumentProfileConnection_getUpdatePeriod")]
+        public static extern long GetUpdatePeriod(
+            nint thread,
+            InstrumentProfileConnectionHandle instrumentProfileConnection);
+
+        [DllImport(
+            ImportInfo.DllName,
+            CallingConvention = CallingConvention.Cdecl,
+            CharSet = CharSet.Ansi,
+            ExactSpelling = true,
+            BestFitMapping = false,
+            ThrowOnUnmappableChar = true,
+            EntryPoint = "dxfg_InstrumentProfileConnection_setUpdatePeriod")]
+        public static extern int SetUpdatePeriod(
+            nint thread,
+            InstrumentProfileConnectionHandle instrumentProfileConnection,
+            long updatePeriod);
+
+        [DllImport(
+            ImportInfo.DllName,
+            CallingConvention = CallingConvention.Cdecl,
+            CharSet = CharSet.Ansi,
+            ExactSpelling = true,
+            BestFitMapping = false,
+            ThrowOnUnmappableChar = true,
+            EntryPoint = "dxfg_InstrumentProfileConnection_getLastModified")]
+        public static extern long GetLasModified(
+            nint thread,
+            InstrumentProfileConnectionHandle instrumentProfileConnection);
+
+        [DllImport(
+            ImportInfo.DllName,
+            CallingConvention = CallingConvention.Cdecl,
+            CharSet = CharSet.Ansi,
+            ExactSpelling = true,
+            BestFitMapping = false,
+            ThrowOnUnmappableChar = true,
+            EntryPoint = "dxfg_InstrumentProfileConnection_start")]
+        public static extern int Start(
+            nint thread,
+            InstrumentProfileConnectionHandle instrumentProfileConnection);
+
+        [DllImport(
+            ImportInfo.DllName,
+            CallingConvention = CallingConvention.Cdecl,
+            CharSet = CharSet.Ansi,
+            ExactSpelling = true,
+            BestFitMapping = false,
+            ThrowOnUnmappableChar = true,
+            EntryPoint = "dxfg_InstrumentProfileConnection_close")]
+        public static extern int Close(
+            nint thread,
+            InstrumentProfileConnectionHandle instrumentProfileConnection);
+    }
+}
