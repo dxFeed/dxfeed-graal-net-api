e9eefce49758020acf023400857df9805b79a4bb
Joshua Berch <voros1@live.com>
2023-11-11 15:41:30 -0500

    Creates DXLink project

---
+++ DXLink.Net/Class1.cs
@@ -0,0 +1,11 @@
+﻿// <copyright file="Class1.cs" company="Devexperts LLC">
+// Copyright © 2022 Devexperts LLC. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+namespace DXLink.Net;
+public class Class1
+{
+
+}
