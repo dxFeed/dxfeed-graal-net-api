181157b2ca7bfaefcab560773547047ca8032b46
Konstantin Ivaschenko <kivaschenko@devexperts.com>
2023-11-24 18:48:28 +0300

    feat: add multiple frameworks

--- publish.sh
+++ publish.sh
@@ -1,38 +1,38 @@
 #!/bin/sh
 
-# Print usage.
-if [ "$#" -eq 0 ]; then
-    echo "Usage:"
-    echo "  <project> [<platform>]"
-    echo "Where:"
-    echo "  project  - Project name."
-    echo "  platform - Platform name (win-x64, linux-x64, osx-x64, osx-arm64)"
-    echo "             for self-contained app."
-    exit 1
-fi
-
-# Publish .NET app.
-if [ "$#" -eq 1 ]; then
-    dotnet publish \
+if [ "$#" -eq 2 ]; then
+    # Publish .NET app.
+    dotnet publish\
         src/"$1"/"$1".csproj\
         -p:UseAppHost=false\
         -p:DebugType=None\
         -p:DebugSymbols=false\
+        --framework "$2"\
         -c Release\
         -o artifacts/Publish/"$1"/
-        exit 0
-fi
-
-# Publish self-contained app for specified platform.
-if [ "$#" -eq 2 ]; then
-    dotnet publish -r "$2"\
+    exit 0
+elif [ "$#" -eq 3 ]; then
+    # Publish self-contained app for specified platform.
+    dotnet publish -r "$3"\
         src/"$1"/"$1".csproj\
         -p:PublishSingleFile=true\
         -p:IncludeNativeLibrariesForSelfExtract=true\
         -p:AllowedReferenceRelatedFileExtensions=none\
-        -p:DebugType=embedded\
+        -p:DebugType=None\
         --self-contained true\
+        --framework "$2"\
         -c Release\
-        -o artifacts/Publish/"$2"/"$1"/
-        exit 0
+        -o artifacts/Publish/"$3"/"$1"/
+    exit 0
+else
+    # Print usage.
+    echo "Usage:"
+    echo "  <project> <framework> [<platform>]"
+    echo ""
+    echo "Where:"
+    echo "  project     Project name."
+    echo "  framework   .NET version (net6.0, net7.0, net8.0)."
+    echo "  platform    Platform name (win-x64, linux-x64, osx-x64, osx-arm64)"
+    echo "              for self-contained app."
+    exit 1
 fi
