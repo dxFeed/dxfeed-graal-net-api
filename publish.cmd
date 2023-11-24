cd315ce1cf26122fe13d936f6fb579a028ab048c
Konstantin Ivaschenko <kivaschenko@devexperts.com>
2023-11-24 18:48:28 +0300

    feat: add multiple frameworks

--- publish.cmd
+++ publish.cmd
@@ -1,38 +1,38 @@
 @echo off
 
-REM Print usage.
-if "%1"=="" (
-    echo "Usage:"
-    echo "  <project> [<platform>]"
-    echo "Where:"
-    echo "  project  - Project name."
-    echo "  platform - Platform name (win-x64, linux-x64, osx-x64, osx-arm64)"
-    echo "             for self-contained app."
-    exit 1
-)
-
-REM Publish .NET app.
-if "%2"=="" (
-    dotnet publish ^
+IF "%3"=="" (
+    REM Publish .NET app.
+    dotnet publish^
         src/"%1"/"%1".csproj^
         -p:UseAppHost=false^
         -p:DebugType=None^
         -p:DebugSymbols=false^
+        --framework "%2"^
         -c Release^
         -o artifacts/Publish/"%1"/
     exit 0
-)
-
-REM Publish self-contained app for specified platform.
-if "%3"=="" (
-    dotnet publish -r "%2"^
+) ELSE IF "%4"=="" (
+    REM Publish self-contained app for specified platform.
+    dotnet publish -r "%3"^
         src/"%1"/"%1".csproj^
         -p:PublishSingleFile=true^
         -p:IncludeNativeLibrariesForSelfExtract=true^
         -p:AllowedReferenceRelatedFileExtensions=none^
-        -p:DebugType=embedded^
+        -p:DebugType=None^
         --self-contained true^
+        --framework "%2"^
         -c Release^
-        -o artifacts/Publish/"%2"/"%1"
+        -o artifacts/Publish/"%3"/"%1"/
     exit 0
+) ELSE (
+    REM Print usage.
+    echo "Usage:"
+    echo "  <project> <framework> [<platform>]"
+    echo ""
+    echo "Where:"
+    echo "  project     Project name."
+    echo "  framework   .NET version (net6.0, net7.0, net8.0)."
+    echo "  platform    Platform name (win-x64, linux-x64, osx-x64, osx-arm64)"
+    echo "              for self-contained app."
+    exit 1
 )
