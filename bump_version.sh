7928f427953ceddfbdf7eb52492b61da9b50971a
Konstantin Ivaschenko <kivaschenko@devexperts.com>
2023-11-30 19:44:24 +0300

    fix: bump_version.sh

--- bump_version.sh
+++ bump_version.sh
@@ -3,3 +3,4 @@ sed -i "" "/<AssemblyVersion>/s/>.*</>""$1""</" Directory.Build.props
 sed -i "" "/<FileVersion>/s/>.*</>""$1"".0</" Directory.Build.props
 sed -i "" "/<Version>/s/>.*</>""$1""</" Directory.Build.props
 sed -i "" "s/nuget-.*-blue/nuget-""$1""-blue/g" README.md
+echo "$1" > version.txt
---
+++ make_release.sh
@@ -0,0 +1,7 @@
+git reset
+git add Directory.Build.props README.md version.txt
+git commit -m "bump version"
+git push origin
+version="$(cat version.txt)"
+git tag "$version"
+git push origin "$version"
