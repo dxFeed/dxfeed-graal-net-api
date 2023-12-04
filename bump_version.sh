a8bb614add5db7a51a9a3537a21e56ce29a44351
Konstantin Ivaschenko <kivaschenko@devexperts.com>
2023-12-04 18:52:10 +0300

    bump version

--- bump_version.sh
+++ bump_version.sh
@@ -4,3 +4,4 @@ sed -i "" "/<FileVersion>/s/>.*</>""$1"".0</" Directory.Build.props
 sed -i "" "/<Version>/s/>.*</>""$1""</" Directory.Build.props
 sed -i "" "s/nuget-.*-blue/nuget-""$1""-blue/g" README.md
 echo "$1" > version.txt
+printf %"s\n\n" "## Version $1" | cat - ReleaseNotes.txt > temp && mv temp ReleaseNotes.txt
