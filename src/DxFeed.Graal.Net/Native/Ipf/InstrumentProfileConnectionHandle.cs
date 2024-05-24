23e7eb190be5b3f05b88517dbbb64815674aead2
Konstantin Ivaschenko <kivaschenko@devexperts.com>
2024-05-24 12:43:14 +0300

    fix: IPF

--- src/DxFeed.Graal.Net/Native/Ipf/InstrumentProfileConnectionHandle.cs
+++ src/DxFeed.Graal.Net/Native/Ipf/InstrumentProfileConnectionHandle.cs
@@ -1,5 +1,5 @@
 // <copyright file="InstrumentProfileConnectionHandle.cs" company="Devexperts LLC">
-// Copyright © 2022 Devexperts LLC. All rights reserved.
+// Copyright © 2024 Devexperts LLC. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 // If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // </copyright>
@@ -45,7 +45,7 @@ internal sealed class InstrumentProfileConnectionHandle : JavaHandle
             EntryPoint = "dxfg_InstrumentProfileConnection_createConnection")]
         public static extern InstrumentProfileConnectionHandle CreateConnection(
             nint thread,
-            [MarshalAs(UnmanagedType.LPUTF8Str)] string address,
+            [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(StringMarshaler))] string address,
             InstrumentProfileCollectorHandle instrumentProfileCollector);
 
         [DllImport(
--- src/DxFeed.Graal.Net/Native/Ipf/InstrumentProfileNative.cs
+++ src/DxFeed.Graal.Net/Native/Ipf/InstrumentProfileNative.cs
@@ -1,5 +1,5 @@
 // <copyright file="InstrumentProfileNative.cs" company="Devexperts LLC">
-// Copyright © 2022 Devexperts LLC. All rights reserved.
+// Copyright © 2024 Devexperts LLC. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 // If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // </copyright>
@@ -11,7 +11,7 @@ using DxFeed.Graal.Net.Native.Interop;
 namespace DxFeed.Graal.Net.Native.Ipf;
 
 [StructLayout(LayoutKind.Sequential)]
-internal readonly record struct InstrumentProfileNative(
+internal record struct InstrumentProfileNative(
     StringNative Type,
     StringNative Symbol,
     StringNative Description,
