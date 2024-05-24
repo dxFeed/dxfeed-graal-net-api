4ff324e52d8e8bd390f2b314aece2b107cd61890
Konstantin Ivaschenko <kivaschenko@devexperts.com>
2024-05-24 12:41:20 +0300

    fix: Events

--- src/DxFeed.Graal.Net/Native/Events/Market/TimeAndSaleNative.cs
+++ src/DxFeed.Graal.Net/Native/Events/Market/TimeAndSaleNative.cs
@@ -1,5 +1,5 @@
 // <copyright file="TimeAndSaleNative.cs" company="Devexperts LLC">
-// Copyright © 2022 Devexperts LLC. All rights reserved.
+// Copyright © 2024 Devexperts LLC. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 // If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // </copyright>
@@ -15,7 +15,7 @@ namespace DxFeed.Graal.Net.Native.Events.Market;
 /// to build an <see cref="TimeAndSale"/>.
 /// </summary>
 [StructLayout(LayoutKind.Sequential)]
-internal readonly record struct TimeAndSaleNative(
+internal record struct TimeAndSaleNative(
     EventTypeNative EventType,
     int EventFlags,
     long Index,
