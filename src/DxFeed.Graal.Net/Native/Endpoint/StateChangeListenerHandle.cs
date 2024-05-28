88cb6dcf3095ff1fa914909086629b7222d5724b
Konstantin Ivaschenko <kivaschenko@devexperts.com>
2024-05-28 20:11:49 +0300

    fix: copyright

--- src/DxFeed.Graal.Net/Native/Endpoint/StateChangeListenerHandle.cs
+++ src/DxFeed.Graal.Net/Native/Endpoint/StateChangeListenerHandle.cs
@@ -1,12 +1,11 @@
 // <copyright file="StateChangeListenerHandle.cs" company="Devexperts LLC">
-// Copyright © 2022 Devexperts LLC. All rights reserved.
+// Copyright © 2024 Devexperts LLC. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 // If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // </copyright>
 
 using System;
 using System.Diagnostics.CodeAnalysis;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using DxFeed.Graal.Net.Native.Interop;
 using static DxFeed.Graal.Net.Api.DXEndpoint;
