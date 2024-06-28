2d0d8e23ad7017282a79b687ce798f0ff5a8ab4f
stdcion <stdcion@gmail.com>
2024-06-28 15:15:19 +0300

    [MDAPI-22] [.NET] CandleWebService API sample (#67)

---
+++ samples/CandleDataResponseReader/CandleMap.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="CandleMap.cs" company="Devexperts LLC">
+// Copyright © 2024 Devexperts LLC. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using CsvHelper.Configuration;
+using DxFeed.Graal.Net.Events.Candles;
+
+namespace DxFeed.Graal.Net.Samples;
+
+/// <summary>
+/// A class map for the <see cref="Candle"/> class, used to map CSV data to <see cref="Candle"/> objects.
+/// </summary>
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global", Justification = "Created by CsvHelper")]
+internal sealed class CandleMap : AbstractTimeSeriesMap<Candle>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CandleMap"/> class.
+    /// This constructor configures the mapping rules for the <see cref="Candle"/> class.
+    /// </summary>
+    public CandleMap()
+    {
+        // Automatically map all fields using the given CSV configuration.
+        AutoMap(new CsvConfiguration(CultureInfo.InvariantCulture) { IgnoreReferences = true });
+        Map(m => m.EventTime).TypeConverter<EventTimeConverter>();
+        Map(m => m.Index).Name("Time").TypeConverter<TimeAndSequenceConverter>();
+        Map(m => m.Time).Ignore(); // Time is processed in Index using TimeAndSequenceConverter.
+        Map(m => m.Sequence).Ignore(); // Sequence is processed in Index using TimeAndSequenceConverter.
+        Map(m => m.EventFlags).Optional().Convert(args => ParseEventFlags(args.Row));
+    }
+}
---
+++ samples/CandleDataResponseReader/TimeAndSaleMap.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="TimeAndSaleMap.cs" company="Devexperts LLC">
+// Copyright © 2024 Devexperts LLC. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using CsvHelper.Configuration;
+using DxFeed.Graal.Net.Events.Market;
+
+namespace DxFeed.Graal.Net.Samples;
+
+/// <summary>
+/// A class map for the <see cref="TimeAndSale"/> class, used to map CSV data to <see cref="TimeAndSale"/> objects.
+/// </summary>
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global", Justification = "Created by CsvHelper")]
+internal sealed class TimeAndSaleMap : AbstractTimeSeriesMap<TimeAndSale>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TimeAndSaleMap"/> class.
+    /// This constructor configures the mapping rules for the <see cref="TimeAndSale"/> class.
+    /// </summary>
+    public TimeAndSaleMap()
+    {
+        // Automatically map all fields using the given CSV configuration.
+        AutoMap(new CsvConfiguration(CultureInfo.InvariantCulture) { IgnoreReferences = true });
+        Map(m => m.EventTime).TypeConverter<EventTimeConverter>();
+        Map(m => m.Index).Name("Time").TypeConverter<TimeAndSequenceConverter>();
+        Map(m => m.Time).Ignore(); // Time is processed in Index using TimeAndSequenceConverter.
+        Map(m => m.Sequence).Ignore(); // Sequence is processed in Index using TimeAndSequenceConverter.
+        Map(m => m.ExchangeSaleConditions).Name("SaleConditions");
+        Map(m => m.Buyer).TypeConverter<StringConverter>();
+        Map(m => m.Seller).TypeConverter<StringConverter>();
+        Map(m => m.EventFlags).Optional().Convert(args => ParseEventFlags(args.Row));
+    }
+}
