377190d1cc30183177bad70595fd58420dc93efd
stdcion <stdcion@gmail.com>
2024-08-08 20:06:35 +0300

    [MDAPI-116] [.NET] Add PriceLevelBookSample (#77)

---
+++ samples/UI/PriceLevelBookSample/PriceLevel.cs
@@ -0,0 +1,80 @@
+﻿// <copyright file="PriceLevel.cs" company="Devexperts LLC">
+// Copyright © 2024 Devexperts LLC. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+using DxFeed.Graal.Net.Events;
+using DxFeed.Graal.Net.Events.Market;
+using DxFeed.Graal.Net.Utils;
+
+namespace PriceLevelBookSample;
+
+/// <summary>
+/// Represents a price level in a price level book.
+/// </summary>
+public class PriceLevel
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PriceLevel"/> class using an order.
+    /// </summary>
+    /// <param name="order">The order to initialize the price level from.</param>
+    public PriceLevel(OrderBase order)
+    {
+        EventSymbol = order.EventSymbol;
+        EventSource = order.EventSource;
+        Side = order.OrderSide;
+        Price = order.Price;
+        Size = order.Size;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PriceLevel"/> class by copying an existing price level.
+    /// </summary>
+    /// <param name="priceLevel">The price level to copy.</param>
+    public PriceLevel(PriceLevel priceLevel)
+    {
+        EventSymbol = priceLevel.EventSymbol;
+        EventSource = priceLevel.EventSource;
+        Side = priceLevel.Side;
+        Price = priceLevel.Price;
+        Size = priceLevel.Size;
+    }
+
+    /// <summary>
+    /// Gets or sets the event symbol.
+    /// </summary>
+    public string? EventSymbol { get; set; }
+
+    /// <summary>
+    /// Gets or sets the event source.
+    /// </summary>
+    public IndexedEventSource EventSource { get; set; }
+
+    /// <summary>
+    /// Gets or sets the side (buy or sell) of the price level.
+    /// </summary>
+    public Side Side { get; set; }
+
+    /// <summary>
+    /// Gets or sets the price of the price level.
+    /// </summary>
+    public double Price { get; set; }
+
+    /// <summary>
+    /// Gets or sets the size of the price level.
+    /// </summary>
+    public double Size { get; set; }
+
+    /// <summary>
+    /// Returns a string that represents the current object.
+    /// </summary>
+    /// <returns>A string that represents the current object.</returns>
+    public override string ToString() =>
+        "PriceLevel{" + StringUtil.EncodeNullableString(EventSymbol) +
+        ", source=" + StringUtil.EncodeNullableString(EventSource.ToString()) +
+        ", side=" + Side +
+        ", price=" + Price +
+        ", size=" + Size +
+        "}";
+}
---
+++ samples/UI/PriceLevelBookSample/PriceLevels.cs
@@ -0,0 +1,85 @@
+﻿// <copyright file="PriceLevels.cs" company="Devexperts LLC">
+// Copyright © 2024 Devexperts LLC. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Avalonia.Controls;
+using Avalonia.Controls.Models.TreeDataGrid;
+using DxFeed.Graal.Net.Events;
+
+namespace PriceLevelBookSample;
+
+/// <summary>
+/// The PriceLevels class is responsible for managing and updating collections of buy and sell price levels.
+/// It provides data sources for binding to UI elements and methods to update these collections.
+/// </summary>
+public class PriceLevels
+{
+    private readonly RangedObservableCollection<PriceLevel> buyPriceLevels = new();
+    private readonly RangedObservableCollection<PriceLevel> sellPriceLevels = new();
+
+    /// <summary>
+    /// Initializes a new instance of the PriceLevels class.
+    /// Sets up the data grid sources for buy and sell price levels.
+    /// </summary>
+    public PriceLevels()
+    {
+        BuyPriceLevels = CreateDataGridSource(buyPriceLevels);
+        SellPriceLevels = CreateDataGridSource(sellPriceLevels);
+    }
+
+    /// <summary>
+    /// Gets the data source for buy price levels.
+    /// </summary>
+    public ITreeDataGridSource<PriceLevel> BuyPriceLevels { get; }
+
+    /// <summary>
+    /// Gets the data source for sell price levels.
+    /// </summary>
+    public ITreeDataGridSource<PriceLevel> SellPriceLevels { get; }
+
+    /// <summary>
+    /// Updates the buy price levels collection with a new set of price levels.
+    /// </summary>
+    /// <param name="priceLevel">The new set of buy price levels.</param>
+    public void UpdateBuy(IEnumerable<PriceLevel> priceLevel) =>
+        buyPriceLevels.ReplaceRange(priceLevel);
+
+    /// <summary>
+    /// Updates the sell price levels collection with a new set of price levels.
+    /// </summary>
+    /// <param name="priceLevel">The new set of sell price levels.</param>
+    public void UpdateSell(IEnumerable<PriceLevel> priceLevel) =>
+        sellPriceLevels.ReplaceRange(priceLevel);
+
+    /// <summary>
+    /// Creates a data grid source from an observable collection of price levels.
+    /// Configures the columns to display price levels details.
+    /// </summary>
+    /// <param name="collection">The observable collection of price levels.</param>
+    /// <returns>A configured FlatTreeDataGridSource for the given collection.</returns>
+    private static FlatTreeDataGridSource<PriceLevel> CreateDataGridSource(ObservableCollection<PriceLevel> collection) =>
+        new(collection)
+        {
+            Columns =
+            {
+                new TextColumn<PriceLevel, int>("№", o => GetRowNumber(collection, o)),
+                new TextColumn<PriceLevel, string>("Symbol", o => o.EventSymbol),
+                new TextColumn<PriceLevel, IndexedEventSource>("Source", o => o.EventSource),
+                new TextColumn<PriceLevel, double>("Price", o => o.Price),
+                new TextColumn<PriceLevel, double>("Size", o => o.Size),
+            }
+        };
+
+    /// <summary>
+    /// Gets the row number of an price levels in the collection.
+    /// </summary>
+    /// <param name="collection">The collection of price levels.</param>
+    /// <param name="priceLevel">The price level whose row number is to be retrieved.</param>
+    /// <returns>The row number of the given price level.</returns>
+    private static int GetRowNumber(ObservableCollection<PriceLevel> collection, PriceLevel priceLevel) =>
+        collection.IndexOf(priceLevel) + 1;
+}
