a413abe4a2baf1c090af1d511d841c13ae593147
stdcion <stdcion@gmail.com>
2025-02-14 16:34:24 +0300

    [MDAPI-171] [.NET] Optimize OptionChainsBuilder for performance in .NET compared to Java (#86)

--- src/DxFeed.Graal.Net/Native/Interop/JavaHandle.cs
+++ src/DxFeed.Graal.Net/Native/Interop/JavaHandle.cs
@@ -1,5 +1,5 @@
 // <copyright file="JavaHandle.cs" company="Devexperts LLC">
-// Copyright © 2024 Devexperts LLC. All rights reserved.
+// Copyright © 2025 Devexperts LLC. All rights reserved.
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 // If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // </copyright>
@@ -78,7 +78,7 @@ internal abstract class JavaHandle : SafeHandle
             return false;
         }
 
-        return Import.Equals(CurrentThread, this, that) != 0;
+        return ErrorCheck.SafeCall(Import.Equals(CurrentThread, this, that)) != 0;
     }
 
     public override string ToString() =>
