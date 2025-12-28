ece65d5b433b966ff58738e5214057aa12e87a77
AnatolyKalin <anatoly.kalin@devexperts.com>
2025-12-28 20:51:52 +0300

    EventTypeNative.Realese + ReleaseList

--- src/DxFeed.Graal.Net/Native/Events/EventTypeNative.cs
+++ src/DxFeed.Graal.Net/Native/Events/EventTypeNative.cs
@@ -4,8 +4,11 @@
 // If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 // </copyright>
 
+using System;
 using System.Runtime.InteropServices;
+using DxFeed.Graal.Net.Native.Graal;
 using DxFeed.Graal.Net.Native.Interop;
+using static DxFeed.Graal.Net.Native.ErrorHandling.ErrorCheck;
 
 namespace DxFeed.Graal.Net.Native.Events;
 
@@ -18,4 +21,38 @@ namespace DxFeed.Graal.Net.Native.Events;
 internal record struct EventTypeNative(
     EventCodeNative EventCode,
     StringNative EventSymbol,
-    long EventTime);
+    long EventTime)
+{
+    public static void Release(IntPtr nativeEvent)
+    {
+        unsafe
+        {
+            SafeCall(Import.Release(Isolate.CurrentThread, (EventTypeNative*)nativeEvent));
+        }
+    }
+
+    public static void ReleaseList(IntPtr nativeEventList)
+    {
+        unsafe
+        {
+            SafeCall(Import.ReleaseList(Isolate.CurrentThread, (ListNative<EventTypeNative>*)nativeEventList));
+        }
+    }
+
+    private static class Import
+    {
+        [DllImport(
+            ImportInfo.DllName,
+            EntryPoint = "dxfg_CList_EventType_release")]
+        public static extern unsafe int ReleaseList(
+            nint thread,
+            ListNative<EventTypeNative>* eventList);
+
+        [DllImport(
+            ImportInfo.DllName,
+            EntryPoint = "dxfg_EventType_release")]
+        public static extern unsafe int Release(
+            nint thread,
+            EventTypeNative* nativeEvent);
+    }
+}
