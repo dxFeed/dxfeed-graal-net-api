c04a5cac568ca7d0107a87dadf4ccf33b5a991c7
AnatolyKalin <anatoly.kalin@devexperts.com>
2025-12-28 21:40:30 +0300

    PriceLevelService

--- src/DxFeed.Graal.Net/Native/Orcs/PriceLevelServiceHandle.cs
+++ src/DxFeed.Graal.Net/Native/Orcs/PriceLevelServiceHandle.cs
@@ -5,8 +5,11 @@
 // </copyright>
 
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using DxFeed.Graal.Net.Events;
+using DxFeed.Graal.Net.Events.Candles;
+using DxFeed.Graal.Net.Events.Market;
 using DxFeed.Graal.Net.Native.ErrorHandling;
 using DxFeed.Graal.Net.Native.Interop;
 using DxFeed.Graal.Net.Native.SymbolMappers;
@@ -21,6 +24,22 @@ internal sealed class PriceLevelServiceHandle : JavaHandle
 
         return service;
     }
+
+    public List<Order> getOrders(CandleSymbol candleSymbol, OrderSource orderSource, TimeSpan from, TimeSpan to, string caller)
+    {
+        // ToDo: implement
+
+        return new List<Order>();
+    }
+
+    public List<Order> getOrders(CandleSymbol candleSymbol, OrderSource orderSource, TimeSpan from, TimeSpan to)
+    {
+        // ToDo: implement
+
+        return new List<Order>();
+    }
+
+    public void Close() => ErrorCheck.SafeCall(NativeClose(CurrentThread, this));
 
     [DllImport(
         ImportInfo.DllName,
@@ -77,4 +96,52 @@ internal sealed class PriceLevelServiceHandle : JavaHandle
         out IntPtr /* ListNative<EventTypeNative>* */ orders);
 
     // ToDo: add dxfg_PriceLevelService_getAuthOrderSource
+
+    [DllImport(
+        ImportInfo.DllName,
+        CallingConvention = CallingConvention.Cdecl,
+        CharSet = CharSet.Ansi,
+        ExactSpelling = true,
+        BestFitMapping = false,
+        ThrowOnUnmappableChar = true,
+        EntryPoint = "dxfg_PriceLevelService_getQuotes")]
+    private static extern int NativeGetQuotes(
+        nint thread,
+        PriceLevelServiceHandle service,
+        [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(SymbolMarshaler))]
+        object candleSymbol,
+        long from,
+        long to,
+        [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(StringMarshaler))]
+        string caller,
+        out IntPtr /* ListNative<EventTypeNative>* */ quotes);
+
+    [DllImport(
+        ImportInfo.DllName,
+        CallingConvention = CallingConvention.Cdecl,
+        CharSet = CharSet.Ansi,
+        ExactSpelling = true,
+        BestFitMapping = false,
+        ThrowOnUnmappableChar = true,
+        EntryPoint = "dxfg_PriceLevelService_getQuotes2")]
+    private static extern int NativeGetQuotes(
+        nint thread,
+        PriceLevelServiceHandle service,
+        [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(SymbolMarshaler))]
+        object candleSymbol,
+        long from,
+        long to,
+        out IntPtr /* ListNative<EventTypeNative>* */ quotes);
+
+    [DllImport(
+        ImportInfo.DllName,
+        CallingConvention = CallingConvention.Cdecl,
+        CharSet = CharSet.Ansi,
+        ExactSpelling = true,
+        BestFitMapping = false,
+        ThrowOnUnmappableChar = true,
+        EntryPoint = "dxfg_PriceLevelService_close")]
+    private static extern int NativeClose(
+        nint thread,
+        PriceLevelServiceHandle service);
 }
---
+++ src/DxFeed.Graal.Net/Orcs/PriceLevelService.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="PriceLevelService.cs" company="Devexperts LLC">
+// Copyright © 2025 Devexperts LLC. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+using System;
+using DxFeed.Graal.Net.Native.Orcs;
+
+namespace DxFeed.Graal.Net.Orcs;
+
+/// <summary>
+///
+/// </summary>
+public class PriceLevelService : IDisposable
+{
+    private readonly PriceLevelServiceHandle handle;
+
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="address"></param>
+    public PriceLevelService(string address) => handle = PriceLevelServiceHandle.Create(address);
+
+    /// <summary>
+    ///
+    /// </summary>
+    public void Close() => handle.Close();
+
+    /// <inheritdoc />
+    public void Dispose() => Close();
+}
