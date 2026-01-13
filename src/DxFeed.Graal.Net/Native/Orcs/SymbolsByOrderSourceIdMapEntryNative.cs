cea23283aa583d41227abc83c29a8be156d67f58
Anatoly Kalin <anatoly.kalin@devexperts.com>
2026-01-13 12:34:48 +0300

    [MDAPI-313][.NET] Implement ORCS API

---
+++ src/DxFeed.Graal.Net/Native/Orcs/SymbolsByOrderSourceIdMapEntryNative.cs
@@ -0,0 +1,15 @@
+﻿// <copyright file="SymbolsByOrderSourceIdMapEntryNative.cs" company="Devexperts LLC">
+// Copyright © 2025 Devexperts LLC. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+// If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+using System.Runtime.InteropServices;
+
+namespace DxFeed.Graal.Net.Native.Orcs;
+
+[StructLayout(LayoutKind.Sequential)]
+internal record struct SymbolsByOrderSourceIdMapEntryNative
+{
+
+}
